eaa9195878cff6804df893b50fe68aaaf1249315
David Engel <Davidjengel2012@gmail.com>
2019-07-16 22:14:39 -0600

    Fix header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import LoginForm from '../containers/LoginForm';
 
 const Header = () => {
   return (
     <header>
       <h1>MOVIE-TRACKER</h1>
-      <NavLink
-          to='/login'
-          className='sign-in-btn'
-          activeClassName='active'
-      >
-        Sign-In
-      </NavLink>
-      <NavLink
-          to='/'
-          className='home-btn'
-          activeClassName='active'
-      >
-        Home
-      </NavLink>
+      <div className='header-btns-container'>
+        <NavLink
+            to='/login'
+            className='sign-in-btn'
+            activeClassName='active'
+        >
+          Sign-In
+        </NavLink>
+        <NavLink
+            to='/'
+            className='home-btn'
+            activeClassName='active'
+        >
+          Home
+        </NavLink>
+      </div>
     </header>
   )
 }
