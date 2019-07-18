dca01ddeff04111a43ea6a7016f3cd558c9a525d
Taylor Jordan <taylornoel418@icloud.com>
2019-07-17 19:12:47 -0600

    Add testing file for actions

---
+++ src/actions/actions.test.js
@@ -0,0 +1,27 @@
+import * as actions from './index';
+
+describe('actions', () => {
+    it('should have a type of LOAD_MOVIES', () => {
+
+    });
+
+    it('should have a type of SIGN_IN', () => {
+
+    });
+
+    it('should have a type of SIGN_OUT', () => {
+
+    });
+
+    it('should have a type of CREATE_ACCT', () => {
+
+    });
+
+    it('should have a type of ADD_FAVORITE', () => {
+
+    });
+
+    it('should have a type of VIEW_FAVORITES', () => {
+        
+    })
+})
