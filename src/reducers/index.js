08feb0875025f583d4819167848a52c58f28b0cd
Vinton Teo <taulago@ymail.com>
2019-07-17 22:25:52 -0600

    Add loadingReducer to store for isLoading state.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
 import { favoritesReducer } from './favoritesReducer';
 import { moviesReducer } from './moviesReducer';
+import { loadingReducer } from './loadingReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   favorites: favoritesReducer,
   movies: moviesReducer,
-  isLoading: true
+  isLoading: loadingReducer
 });
 
 export default rootReducer;
