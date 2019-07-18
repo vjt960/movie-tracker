63a5fd580359d04834af3531c5be2a2a3bc8d174
Vinton Teo <taulago@ymail.com>
2019-07-17 22:23:33 -0600

    Create loadComplete action function.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,30 @@
 export const loadMovies = movies => ({
-    type: 'LOAD_MOVIES',
-    movies
+  type: 'LOAD_MOVIES',
+  movies
 });
 
-export const signIn = (user) => ({
-    type: 'SIGN_IN',
-    user
-}); 
+export const loadComplete = () => ({
+  type: 'LOAD_COMPLETE'
+});
+
+export const signIn = user => ({
+  type: 'SIGN_IN',
+  user
+});
 
 export const signOut = () => ({
-    type: 'SIGN_OUT'
+  type: 'SIGN_OUT'
 });
 
 export const createAcct = (name, email, password) => ({
-    type: 'CREATE_ACCT',
-    name,
-    email,
-    password
+  type: 'CREATE_ACCT',
+  name,
+  email,
+  password
 });
 
-export const addFavorite = () => ({
+export const addFavorite = () => ({});
 
-});
+export const viewFavorites = () => ({});
 
-export const viewFavorites = () => ({
-
-});
-
-export const deleteFavorite = () => ({
-
-});
+export const deleteFavorite = () => ({});
