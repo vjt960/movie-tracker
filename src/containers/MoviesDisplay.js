f0ff6bd8553184e99ce8eea1cf575e2491405cbb
Vinton Teo <taulago@ymail.com>
2019-07-17 22:44:06 -0600

    Format MoviesDisplay to fix linting errors.

--- src/containers/MoviesDisplay.js
+++ src/containers/MoviesDisplay.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import MoviePoster from '../components/MoviePoster';
 
-
 const MoviesDisplay = ({ movies }) => {
   const allMovies = movies.map(movie => {
-    return(
-      <MoviePoster title={movie.title} posterPath={movie.poster_path} key={movie.id} releaseDate={movie.release_date} />
-    )
-  })
-  return (
-    <section className='movies-display'>
-      {allMovies}
-    </section>
-    
-  )
-}
+    return (
+      <MoviePoster
+        title={movie.title}
+        posterPath={movie.poster_path}
+        key={movie.id}
+        releaseDate={movie.release_date}
+      />
+    );
+  });
+  return <section className="movies-display">{allMovies}</section>;
+};
 
 const mapStateToProps = state => {
-  return { movies: state.movies[0] }
-}
+  return { movies: state.movies[0] };
+};
 
 export default connect(mapStateToProps)(MoviesDisplay);
