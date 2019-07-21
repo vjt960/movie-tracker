682359523f8e3b6dae5383a1bcc3325188edc73a
David Engel <Davidjengel2012@gmail.com>
2019-07-21 13:48:34 -0600

    Get movieDetails displaying information onHover, almost done with this functionality
    Co-authored-by: Taylor Jordan <taylornoel418@icloud.com

--- src/components/movieDetailsDisplay.js
+++ src/components/movieDetailsDisplay.js
@@ -4,14 +4,20 @@ const MovieDetailsDisplay = ({ foundMovie }) => {
   console.log('movie', foundMovie)
   return (
     <section className='movie-details-display '>
-      {foundMovie && 
-        <p className='details-title'>{foundMovie.title}</p>}
-      {foundMovie && 
-        <p className='details-overview'>{foundMovie.overview}</p>}
-      {foundMovie && 
-        <p className='details-vote-average'>{foundMovie.vote_average}</p>}
-      {foundMovie && 
-        <p className='details-release-date'>{foundMovie.release_date}</p>}
+      {foundMovie.backdrop &&
+        <img 
+        src={`https://image.tmdb.org/t/p/w500/${foundMovie.backdrop}`} alt='movie-backdrop' 
+        className='details-img'/>}
+      <article>
+        {foundMovie && 
+          <h2 className='details details-title'>{foundMovie.title}</h2>}
+        {foundMovie && 
+          <p className='details details-overview'>{foundMovie.overview}</p>}
+        {foundMovie.vote_average && 
+          <p className='details details-vote-average'>Average Rating:  {foundMovie.vote_average}/10</p>}
+        {foundMovie.release_date && 
+          <p className='details details-release-date'>Release Date:  {foundMovie.release_date}</p>}
+      </article>
     </section>
   )
 }
