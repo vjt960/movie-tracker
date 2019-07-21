226cc4f22128be0f59ce3a0007fe0c9c9c66a9ff
Taylor Jordan <taylornoel418@icloud.com>
2019-07-20 22:18:24 -0600

    Update red tests for fetchMovieData

--- src/utilz/apiCalls.test.js
+++ src/utilz/apiCalls.test.js
@@ -5,19 +5,12 @@ describe('fetchMovieData', () => {
     {
       vote_count: 1888,
       id: 429617,
-      video: false,
       vote_average: 7.8,
       title: 'Spider-Man: Far from Home',
-      popularity: 417.77,
       poster_path: '/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg',
-      original_language: 'en',
-      original_title: 'Spider-Man: Far from Home',
-      genre_ids: [28, 12, 878],
       backdrop_path: '/dihW2yTsvQlust7mSuAqJDtqW7k.jpg',
-      adult: false,
       overview:
         'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.',
-      release_date: '2019-06-28'
     }
   ];
 
@@ -38,9 +31,8 @@ describe('fetchMovieData', () => {
   });
 
   it('HAPPY: should return a response if promise resolves', async () => {
-    const result = await fetchMovieData();
-
-    expect(result).toEqual(mockMovies);
+    await expect(fetchMovieData()).resolves.toEqual(mockMovies);
+    
   });
 
   it('SAD: should throw an error if promise rejects', async () => {
@@ -54,4 +46,45 @@ describe('fetchMovieData', () => {
       Error('Error fetching movies')
     );
   });
-});
+})
+
+
+
+
+//   describe('fetchUser', () => {
+//     let email;
+//     let password
+
+//     beforeEach(() => {
+//         email = 'email'
+//         password = 'password'
+
+//       window.fetch = jest.fn().mockImplementation(() => {
+//         Promise.resolve({
+//           ok: true,
+//           json: Promise.resolve(user)
+//         })
+//       })
+//     })
+//     it('should call fetch with appropriate params',  () => {
+//       const options = {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ email: email, password: password })
+//         }
+
+//       const url = 'http://localhost:3000/api/users'
+
+//       fetchUser()
+    
+//       expect(window.fetch).toHaveBeenCalledWith(url, options)
+//   })
+
+// });
+
+// describe('postNewUser', () => {
+
+// })
+
+
+
