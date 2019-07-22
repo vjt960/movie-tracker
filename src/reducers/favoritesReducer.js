adf9f53d69d28a5238535524e16fdfe497647d85
Vinton Teo <taulago@ymail.com>
2019-07-22 00:02:14 -0600

    Remove all switch cases to replace with LOAD_FAVORITES case.

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,13 +1,7 @@
 export const favoritesReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_FAVORITE':
-      return [...state, action.movie];
-
-    case 'VIEW_FAVORITES':
-      return action.allFaves;
-
-    case 'DELETE_FAVORITE':
-      return state.filter(fave => fave.id !== action.id);
+    case 'LOAD_FAVORITES':
+      return action.movies;
 
     default:
       return state;
