da86358a9fb3031cca52d42fd06a058442453299
Vinton Teo <taulago@ymail.com>
2019-07-22 00:00:11 -0600

    Fix LOAD_MOVIES case to spread movies array within state.

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -1,19 +1,19 @@
 export const moviesReducer = (state = [], action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'LOAD_MOVIES':
-      return [...state, action.movies];    
+      return [...state, ...action.movies];
     default:
       return state;
   }
-}
+};
 
 export const focusMovieReducer = (state = {}, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'FOCUS_MOVIE':
-      return action.movie; 
+      return action.movie;
     case 'CANCEL_FOCUS':
-      return state = {};
+      return (state = {});
     default:
       return state;
   }
-}
+};
