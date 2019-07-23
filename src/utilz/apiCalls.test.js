1eed1924b01ac3257c5b827cb9b54e47016d4b90
Vinton Teo <taulago@ymail.com>
2019-07-22 21:59:11 -0600

    Structure describe blocks for tests.

---
+++ src/utilz/apiCalls.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  fetchMovieData,
+  fetchFavorites,
+  fetchUser,
+  postNewUser,
+  postFavorite,
+  removeFavorite
+} from './apiCalls';
+
+describe('apiCalls', () => {
+  describe('fetchMovieData', () => {
+    //
+  });
+
+  describe('fetchFavorites', () => {
+    //
+  });
+
+  describe('fetchUser', () => {
+    //
+  });
+
+  describe('postNewUser', () => {
+    //
+  });
+
+  describe('postFavorite', () => {
+    //
+  });
+
+  describe('removeFavorite', () => {
+    //
+  });
+});
